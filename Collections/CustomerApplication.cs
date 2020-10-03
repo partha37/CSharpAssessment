24be013813f7b3b70356b2ba037045a3147e2f27
Parthiban <parthi37cse@gmail.com>
2020-10-03 16:00:07 +0530

    Initial
    Commit

---
+++ Collections/CustomerApplication.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    public class Customer
+    {
+        public Customer(int customerID, string name, string address, double hotelPrice)
+        {
+            CustomerID = customerID;
+            Name = name;
+            Address = address;
+            HotelPrice = hotelPrice;
+        }
+
+        public int CustomerID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public double HotelPrice { get; set; }
+    }
+    interface ICustomerManager
+    {
+        bool AddCustomer(Customer bk);
+        bool DeleteCustomer(int id);
+        Customer[] FindCustomers(string Name);
+        bool UpdateCustomer(int id);
+        bool ViewAllCustomers();
+    }
+    class CustomerManager : ICustomerManager
+    {
+        HashSet<Customer> Customers = new HashSet<Customer>();
+        public bool AddCustomer(Customer bk)
+        {
+            return Customers.Add(bk);
+        }
+
+        public bool DeleteCustomer(int id)
+        {
+            foreach (Customer bk in Customers)
+            {
+                if (bk.CustomerID == id)
+                {
+                    Customers.Remove(bk);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public Customer[] FindCustomers(string Name)
+        {
+            List<Customer> findlist = new List<Customer>();
+            foreach (Customer bk in Customers)
+            {
+                if (bk.Name.Contains(Name))
+                {
+                    findlist.Add(bk);
+                }
+            }
+            return findlist.ToArray();
+        }
+
+        public bool UpdateCustomer(int id)
+        {
+            foreach (Customer bk in Customers)
+            {
+                if (bk.CustomerID == id)
+                {
+                    Console.Write("Enter the New Name: ");
+                    string newName = Console.ReadLine();
+                    Console.Write("Enter the New Address: ");
+                    string newAddress = Console.ReadLine();
+                    Console.Write("Enter the New HotelPrice: ");
+                    double newHotelPrice = double.Parse(Console.ReadLine());
+                    bk.Name = newName;
+                    bk.Address = newAddress;
+                    bk.HotelPrice = newHotelPrice;
+                    return true;
+                }
+            }
+            return false;
+        }
+        public bool ViewAllCustomers()
+        {
+            foreach (var bk in Customers)
+            {
+                Console.WriteLine("-------------------------------");
+                Console.WriteLine($"Customer ID: {bk.CustomerID}");
+                Console.WriteLine($"Name: {bk.Name}");
+                Console.WriteLine($"Address: {bk.Address}");
+                Console.WriteLine($"HotelPrice: {bk.HotelPrice}");
+                Console.WriteLine("-------------------------------");
+            }
+            return true;
+        }
+        public void PrintFind(Customer[] temp)
+        {
+            Console.WriteLine("The Record Found are: \n");
+            foreach (var item in temp)
+            {
+                Console.WriteLine(item.CustomerID);
+                Console.WriteLine(item.Name);
+                Console.WriteLine(item.Address);
+                Console.WriteLine(item.HotelPrice+"\n");
+            }
+
+        }
+
+        
+        
+    }
+
+    class PrintMenu
+    {
+        
+        public static void MyConsole()
+        {
+            Customer bk = new Customer(82, "padhu", "Vellore", 4000);
+            Customer bk2 = new Customer(83, "parthi", "Mysore", 8000);
+            CustomerManager mgr = new CustomerManager();
+            mgr.AddCustomer(bk);
+            mgr.AddCustomer(bk2);
+
+            do
+            {
+                Console.WriteLine("\n----Menu-----");
+                Console.WriteLine("1.View All Customers\n2.Add Customer\n3.Find Customer\n4.Update Customer\n5.Delete Customer\n6.Exit");
+                int choice = Int32.Parse(Console.ReadLine());
+                if (choice == 1)
+                {
+                    mgr.ViewAllCustomers();
+                }
+                else if (choice == 2)
+                {
+                    Console.WriteLine("Enter The CustomerId:  ");
+                    int id = Int32.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter The Customer name:  ");
+                    string name = (Console.ReadLine());
+                    Console.WriteLine("Enter The Customer Address:  ");
+                    string Address = (Console.ReadLine());
+                    Console.WriteLine("Enter The Hote Price:  ");
+                    int price = Int32.Parse(Console.ReadLine());
+                    Customer bk1 = new Customer(id,name,Address,price);
+                    mgr.AddCustomer(bk1);
+                }
+                else if (choice == 3)
+                {
+                    Console.WriteLine("Enter the name to Find: ");
+                    string str = Console.ReadLine();
+                    Customer[] temp = mgr.FindCustomers(str);
+                    mgr.PrintFind(temp);
+                }
+                else if (choice == 4)
+                {
+                    Console.WriteLine("Enter the Customer ID: ");
+                    int id = Int32.Parse( Console.ReadLine());
+                    mgr.UpdateCustomer(id);
+                }
+                else if (choice == 5)
+                {
+                    Console.WriteLine("Enter the Customer ID: ");
+                    int id = Int32.Parse(Console.ReadLine());
+                    mgr.DeleteCustomer(id);
+                }
+                else if(choice==6)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Enter The Correct Number");
+                }
+
+            } while (true);
+        }
+    }
+
+    class CustomerCallManager
+    {
+        static void Main(string[] args)
+        {
+            PrintMenu.MyConsole();
+        }
+
+    }
+}
---
+++ Serialization/SerialDeserial.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Serialization
+{
+    [Serializable]
+    class Employee
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public string Address { get; set; }
+        public override string ToString()
+        {
+            return string.Format($"Name: {Name}\nID: {Id}\nAddress: {Address}");
+        }
+    }
+
+    class ConsoleInvoke
+    {
+        public static void BinarySerialization()
+        {
+
+            do
+            {
+                Console.Write("\nDo You Want to Read or Write? (r/w): \n");
+                Console.Write("Pressing any other keys will exit the program!!!!\n");
+                string choice = Console.ReadLine();
+
+                if (choice.ToLower() == "w")
+                    SerialDeserial.serialize();
+                else if (choice.ToLower() == "r")
+                    SerialDeserial.deserialize();
+                else
+                    break;
+            } while (true);
+        }
+    }
+    class SerialDeserial
+    {
+        public static void serialize()
+        {
+            Employee emp = new Employee { Name = "Parthiban", Id = 14, Address = "Mysore" };
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream fs = new FileStream("FileSave.bin", FileMode.OpenOrCreate, FileAccess.Write);
+            bf.Serialize(fs, emp);
+            Console.WriteLine("\nThe Contents of File Are Written to the File");
+            Console.WriteLine("-----------------------------------------------");
+            fs.Close();
+        }
+
+        public static void deserialize()
+        {
+            FileStream fs = new FileStream("FileSave.bin", FileMode.Open, FileAccess.Read);
+            BinaryFormatter bf = new BinaryFormatter();
+            Employee emp = bf.Deserialize(fs) as Employee;
+            Console.WriteLine("\nThe Contents of File After Reading");
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine($"Employee Name: {emp.Name}");
+            Console.WriteLine($"Employee Name: {emp.Id}");
+            Console.WriteLine($"Employee Name: {emp.Address}");
+            fs.Close();
+        }
+    }
+    class BinarySerialization
+    {
+        static void Main(string[] args)
+        {
+            ConsoleInvoke.BinarySerialization();
+        }
+
+    }
+}
